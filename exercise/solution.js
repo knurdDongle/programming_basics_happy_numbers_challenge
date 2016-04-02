84078bafea7565372f742c963c56144d5e47856a
eye <maoeye18@gmail.com>
2016-04-02 13:25:30 +0300

    add implementation of happyNumber function

---
+++ exercise/solution.js
@@ -0,0 +1,33 @@
+const happyNumber = function(number)
+{
+// BEGIN (write your solution here)
+  const sum = function(num)
+  {
+    let arr = String(num).split('');
+    let sum = 0;
+    arr.forEach(
+      function(elm)
+      {
+        sum = sum + elm * elm;
+      });
+    return sum;
+  };
+
+  let arr = [];
+  let res = number;
+  for (let i = 0; true; i++) {
+    res = sum(res);
+    if (res == 1)
+    {
+      return true;
+    }
+    if (arr.indexOf(res) > -1)
+    {
+      return false;
+    }
+    arr.push(res);
+  }
+// END
+};
+
+export default happyNumber;
